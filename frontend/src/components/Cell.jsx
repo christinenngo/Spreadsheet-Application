24164fb870e1850b2460592a1515686f546bf4ff
Christine Ngo <cnngo@wpi.edu>
2026-06-24 12:29:50 -0400

    Added a toolbar, error indicators, and an import/export to JSON feature.

--- frontend/src/components/Cell.jsx
+++ frontend/src/components/Cell.jsx
@@ -8,7 +8,7 @@ import {memo, useState} from "react";
 //  returns a memoized react component, which only re-renders if its props have changed,
 //  when parent component re-renders
 
-const Cell = memo(({row, col, cellData, onSave}) => { // re-render
+const Cell = memo(({row, col, cellData, onSave, onSelect, selectedCell, selectedGroups, referencedCells, error}) => { // re-render
         const [isEditing, setisEditing] = useState(false);
         const [editValue, setEditValue] = useState('');
 
@@ -35,26 +35,71 @@ const Cell = memo(({row, col, cellData, onSave}) => { // re-render
             }
         }
 
+        const isSelected = selectedCell?.row === row && selectedCell?.col === col;
+
+        const group = selectedGroups.find(g =>
+            row >= g.startRow && row <= g.endRow && col >= g.startCol && col <= g.endCol
+        )
+        let isGroup = !!group;
+
+        const top = isGroup && row === group.startRow;
+        const bottom = isGroup && row === group.endRow;
+        const left = isGroup && col === group.startCol;
+        const right = isGroup && col === group.endCol;
+
+        const cellRef = !isGroup && referencedCells.find(c =>
+            c.row === row && c.col === col
+        )
+        const isReference = !!cellRef;
+
+        const groupStyle = isGroup ?
+            `${group.color.bg}
+             ${top ?  `border-t-2 [border-top-style:dashed] ${group.color.border}` : "border-t-gray-300"}
+             ${bottom ?  `border-b-2 [border-bottom-style:dashed] ${group.color.border}` : "border-b-gray-300"}
+             ${left ?  `border-l-2 [border-left-style:dashed] ${group.color.border}` : "border-l-gray-300"}
+             ${right ?  `border-r-2 [border-right-style:dashed] ${group.color.border}` : "border-r-gray-300"}` : "";
+
+        const cellRefStyle = isReference ?
+            `${cellRef.color.bg} ${cellRef.color.border} border-dashed border-2` : "";
+
+        const errorStyle = "absolute top-0 right-0 z-20 w-0 h-0 border-t-0 border-l-0 border-r-8 border-b-8 border-t-transparent border-b-transparent border-solid border-r-red-500";
+
+        const coloredCell = isSelected || isGroup || isReference;
+
         if (isEditing) {
-            return (<td className="border border-gray-300 p-0 min-w-[100px] h-[30px]">
+            return (
+                <td onClick={() => onSelect(row, col, cellData)}
+                    className={`border ${coloredCell ? "" : "border-gray-300 hover:bg-gray-50"} p-0 min-w-[100px] h-[30px]
+                        ${isSelected ? "border-blue-400 bg-blue-50 border-2" : ""}
+                        ${groupStyle}
+                        ${cellRefStyle}`}>
                 <input
-                ref={editMode}
-                value={editValue}
-                onChange={e => setEditValue(e.target.value)}
-                onBlur={handleOutBlur}
-                onKeyDown={handleKeyEnter}
-                type="text"
-                className="w-full h-full px-2 py-1 outline-none border-2 border-blue-500 z-10 relative"
+                    ref={editMode}
+                    value={editValue}
+                    onChange={e => setEditValue(e.target.value)}
+                    onBlur={handleOutBlur}
+                    onKeyDown={handleKeyEnter}
+                    type="text"
+                    className="w-full h-full px-2 py-1 outline-none border-2 border-blue-500 z-10 relative"
                 />
             </td>)
         }
 
-        return (<td onDoubleClick={handleDoubleClick}
-                    className="border border-gray-300 min-w-[100px] h-[30px] px-2 py-1 cursor-text hover:bg-gray-50 relative overflow-hidden whitespace-nowrap">
-            {cellData.value}
-        </td>)
-
-
+        return (
+            <td onDoubleClick={handleDoubleClick}
+                onClick={() => onSelect(row, col, cellData)}
+                className={`border ${coloredCell ? "" : "border-gray-300 hover:bg-gray-50"} min-w-[100px] h-[30px] px-2 py-1 cursor-text relative overflow-hidden whitespace-nowrap
+                    ${isSelected ? "border-blue-400 bg-blue-50 border-2" : ""}
+                    ${groupStyle}
+                    ${cellRefStyle}`}>
+                {cellData.value}
+                {error && (
+                    <div
+                        title={error}
+                        className={`${errorStyle}`}
+                    ></div>
+                )}
+            </td>)
 })
 
 export default Cell;
--- frontend/src/components/ReactTable.jsx
+++ frontend/src/components/ReactTable.jsx
@@ -2,11 +2,22 @@ import Cell from './Cell';
 import {initialGrid, colLabels} from "./CellDefs";
 import React, {useCallback, useState, useEffect} from "react";
 
-
-
 const ReactSpreadSheet = () => {
 
     const [data, setData] = useState(initialGrid);
+    const [selectedCell, setSelectedCell] = useState(null);
+    const [selectedGroups, setSelectedGroups] = useState([]);
+    const [expressionBar, setExpressionBar] = useState("");
+    const [referencedCells, setReferencedCells] = useState([]);
+    const [cellErrors, setCellErrors] = useState({})
+    const colors = [
+        {border: 'border-pink-500', bg: 'bg-pink-100'},
+        {border: 'border-green-500', bg: 'bg-green-100'},
+        {border: 'border-red-500', bg: 'bg-red-100'},
+        {border: 'border-purple-500', bg: 'bg-purple-100'},
+        {border: 'border-teal-500', bg: 'bg-teal-100'},
+        {border: 'border-orange-500', bg: 'bg-orange-100'}
+    ]
 
     // fetches the entire spreadsheet
         const fetchSpreadsheet = useCallback(() => {
@@ -22,7 +33,7 @@ const ReactSpreadSheet = () => {
             fetchSpreadsheet();
         }, [fetchSpreadsheet]);
 
-    const handleSaveCell = async (row, col, cellData,) => {
+    const handleSaveCell = async (row, col, cellData, updateSheet = true) => {
         const rowNum = row+1; // 0 counting to 1 counting
         const coord = `${col}${rowNum}`;
 
@@ -46,58 +57,266 @@ const ReactSpreadSheet = () => {
             if (!response.ok) { // serverside error
                 const errorData = await response.json();
                 console.error(`Update failed for ${coord}:`, errorData.error);
-                alert(`Error updating cell ${coord}: ${errorData.error || response.statusText}`);
-                return; // error -> don't refresh data
+
+                setCellErrors(prev => ({
+                    ...prev,
+                    [coord]: `Error updating cell ${coord}: ${errorData.error || response.statusText}`
+                }))
+                return false; // error -> don't refresh data
             }
 
-            console.log(`Cell ${coord} updated successfully. Refreshing spreadsheet.`)
-            fetchSpreadsheet();// no error -> refresh data
+            setCellErrors(prev => ({
+                ...prev,
+                [coord]: undefined
+            }));
+
+            console.log(`Cell ${coord} updated successfully.`)
+
+            if (updateSheet) {
+                fetchSpreadsheet();// no error -> refresh data
+            }
         }
         catch (err) {
             console.error("Fetch error during cell update:", err);
-            alert("Network error updating cell.");
+            setCellErrors(prev => ({
+                ...prev,
+                [coord]: "Network error updating cell."
+            }))
+            return true;
+        }
+    }
+
+    const handleUndo = async () => {
+        try {
+            const response = await fetch(`/api/undo`, {
+                method: 'POST'
+            });
+
+            if (!response.ok) {
+                console.error(`Undo failed.`);
+                alert(`Error undoing last action.`);
+                return;
+            }
+
+            console.log(`Successfully undid last action. Refreshing spreadsheet.`)
+            fetchSpreadsheet();// no error -> refresh data
+        }
+        catch (err) {
+            console.error("Fetch error during undo:", err);
+            alert("Error undoing previous action.");
+        }
+    }
+
+    const handleRedo = async () => {
+        try {
+            const response = await fetch(`/api/redo`, {
+                method: 'POST'
+            });
+
+            if (!response.ok) {
+                console.error(`Redo failed.`);
+                alert(`Error redoing last action.`);
+                return;
+            }
+
+            console.log(`Successfully redid last action. Refreshing spreadsheet.`)
+            fetchSpreadsheet();// no error -> refresh data
+        }
+        catch (err) {
+            console.error("Fetch error during redo:", err);
+            alert("Error redoing previous action.");
+        }
+    }
+
+    const handleSelectCell = async (row, col, cellData) => {
+        setSelectedCell({row, col});
+
+        let expression = "";
+
+        if (cellData.expression !== "") {
+            expression = cellData.expression;
+            setExpressionBar(expression);
+        } else {
+            setExpressionBar(cellData.value);
+        }
+
+        const references = expression.match(/[A-Z]+\d+:[A-Z]+\d+|[A-Z]+\d+/g) || [];
+        const groups = [];
+        const cells = [];
+
+        references.forEach((ref, index) => {
+            const color = colors[index % colors.length];
+
+            if (ref.includes(':')) {
+                groups.push({...findRange(ref), color});
+            } else {
+                const row = parseInt(ref.match(/\d+/)[0]) - 1;
+                const col = ref.match(/[A-Z]+/)[0];
+
+                cells.push({row, col, color})
+            }
+        })
+
+        setReferencedCells(cells);
+        setSelectedGroups(groups);
+    }
+
+    const handleNewExpression = () => {
+        if (!selectedCell) {
+            return;
+        }
+
+        handleSaveCell(selectedCell.row, selectedCell.col, expressionBar, true);
+    }
+
+    const findRange = (cellGroup) => {
+        const [start, end] = cellGroup.split(":");
+
+        const startRow = parseInt(start.match(/\d+/)[0]) - 1;
+        const startCol = start.match(/[A-Z]+/)[0];
+        const endRow = parseInt(end.match(/\d+/)[0]) - 1;
+        const endCol = end.match(/[A-Z]+/)[0];
+
+        return {startRow, startCol, endRow, endCol};
+    }
+
+    const downloadFile = (data, fileName, fileType) => {
+        const blob = new Blob([data], {type: fileType});
+
+        const a = document.createElement('a');
+        a.download = fileName;
+        a.href = window.URL.createObjectURL(blob);
+
+        const clickEvent = new MouseEvent('click', {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+        })
+
+        a.dispatchEvent(clickEvent);
+        a.remove();
+    }
+
+    const exportToJson = (e) => {
+        e.preventDefault();
+        downloadFile(JSON.stringify(data, null, 2), 'spreadsheet-data.json', 'text/json');
+    }
+
+    const importJson = async (e) => {
+        const file = e.target.files[0];
+
+        try {
+            const content = await file.text();
+            const rows = JSON.parse(content);
+
+            for (const row of data) {
+                const rowIndex = row.id - 1;
+                for (const col of colLabels) {
+                    const cellData = row[col];
+                    if (cellData?.value || cellData?.expression) {
+                        await handleSaveCell(rowIndex, col, '', false);
+                    }
+                }
+            }
+
+            for (const row of rows) {
+                const rowIndex = row.id - 1;
+                for (const col of colLabels) {
+                    const cellData = row[col];
+                    if (!cellData) {
+                        continue;
+                    }
+
+                    const value = cellData.expression || cellData.value;
+                    if (!value) {
+                        continue;
+                    }
+
+                    await handleSaveCell(rowIndex, col, value, false);
+                }
+            }
+
+            fetchSpreadsheet();
+        } catch (err) {
+            console.error("Failed to import file:", err);
+            alert("Invalid file.")
+        } finally {
+            e.target.value = ''
         }
     }
 
     return(
         <div className="overflow-auto h-screen w-full bg-white text-sm font-sans">
-        <table className="border-collapse w-full">
-            <thead>
-            <tr>
-                {/* Corner Cell */}
-                <th className="bg-gray-100 border border-gray-300 w-10 sticky left-0 top-0 z-20"></th>
-                { colLabels.map((label) => (
-                    <th
-                    key={label}
-                    className="bg-gray-100 border border-gray-300 px-2 py-1 font-normal text-gray-600 min-w-[100px] sticky top-0 z-10">
-                        {label}
-                    </th>
-                    ))}
-            </tr>
-            </thead>
-            <tbody>
-            {data.map((row, rowIndex) => (
-                <tr key={row.id}>
-                    {/* Row Number */}
-                    <td className="bg-gray-100 border border-gray-300 text-center text-gray-500 w-10 sticky left-0 z-10 select-none">
-                        {row.id}
-                    </td>
-                    {/* Data Cells */}
-                    {colLabels.map((colId) => (
-                        <Cell
-                            key={`${rowIndex}-${colId}`}
-                            row={rowIndex}
-                            col={colId}
-                            cellData={row[colId]}
-                            onSave={handleSaveCell}
-                        />
-                    ))}
-                </tr>
-            ))}
-            </tbody>
-        </table>
+            <div className="bg-gray-300 px-6 py-3 font-normal text-gray-700 flex gap-3">
+                <button className="bg-gray-100 border-2 rounded-lg border-gray-400 px-2 py-1"
+                        onClick={handleUndo}>Undo</button>
+                <button className="bg-gray-100 border-2 rounded-lg border-gray-400 px-2 py-1"
+                        onClick={handleRedo}>Redo</button>
 
-    </div>)
+                <div className="px-5 py-1 italic font-medium">fx</div>
+                <input className="border border-gray-400 px-2 py-1 flex-1"
+                       value={expressionBar}
+                       onChange={(e) => {setExpressionBar(e.target.value)}}
+                       onKeyPress={(e) => {
+                           if (e.key === "Enter") {
+                               handleNewExpression()
+                           }
+                       }}
+                />
+
+                <label className="bg-gray-100 border-2 rounded-lg border-gray-400 px-2 py-1 cursor-pointer">
+                    Import Data
+                    <input
+                        className="hidden"
+                        type="file"
+                        accept="application/json"
+                        onChange={importJson}/>
+                </label>
+                <button className="bg-gray-100 border-2 rounded-lg border-gray-400 px-2 py-1"
+                        onClick={exportToJson}>Export Data</button>
+            </div>
+
+            <table className="border-collapse w-full">
+                <thead>
+                <tr>
+                    {/* Corner Cell */}
+                    <th className="bg-gray-100 border border-gray-300 w-10 sticky left-0 top-0 z-20"></th>
+                    { colLabels.map((label) => (
+                        <th
+                        key={label}
+                        className="bg-gray-100 border border-gray-300 px-2 py-1 font-normal text-gray-600 min-w-[100px] sticky top-0 z-10">
+                            {label}
+                        </th>
+                        ))}
+                </tr>
+                </thead>
+                <tbody>
+                {data.map((row, rowIndex) => (
+                    <tr key={row.id}>
+                        {/* Row Number */}
+                        <td className="bg-gray-100 border border-gray-300 text-center text-gray-500 w-10 sticky left-0 z-10 select-none">
+                            {row.id}
+                        </td>
+                        {/* Data Cells */}
+                        {colLabels.map((colId) => (
+                            <Cell
+                                key={`${rowIndex}-${colId}`}
+                                row={rowIndex}
+                                col={colId}
+                                cellData={row[colId]}
+                                onSave={handleSaveCell}
+                                onSelect={handleSelectCell}
+                                selectedCell={selectedCell}
+                                selectedGroups={selectedGroups}
+                                referencedCells={referencedCells}
+                                error={cellErrors[`${colId}${rowIndex+1}`]}
+                            />
+                        ))}
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>)
 }
 
 export default ReactSpreadSheet;
